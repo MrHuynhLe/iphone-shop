7f7cef523fb7755a6bd6364ed0c457043e3718a6
huynhlvph36158 <huynhlvph36158@fpt.edu.vn>
2025-09-29 14:27:11 +0700

    c

---
+++ iphone-shop/src/main/java/com/vn/iphoneshop/IphoneShopApplication.java
@@ -0,0 +1,13 @@
+package com.vn.iphoneshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IphoneShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IphoneShopApplication.class, args);
+    }
+
+}
---
+++ iphone-shop/src/test/java/com/vn/iphoneshop/IphoneShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.iphoneshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IphoneShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
